bc7aa310efdf6f4108adcd7ed64f437ed2db4f2d
saens <songsh5775@gmail.com>
2023-08-10 19:19:24 +0900

    modified theme logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_sns_practice/data/data_theme.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_sns_practice/pages/page_login.dart';
 import 'package:flutter_sns_practice/pages/page_register.dart';
 import 'package:go_router/go_router.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
-
+class MyApp extends ConsumerWidget {
   final GoRouter _router = GoRouter(
     routes: [
       GoRoute(
@@ -25,16 +24,25 @@ class MyApp extends StatelessWidget {
 
   MyApp({super.key});
 
+  final seedColorProvider = StateProvider((ref) => Colors.red);
+
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    Color themeSeedColor = ref.watch(seedColorProvider);
     return MaterialApp.router(
       routerDelegate: _router.routerDelegate,
       routeInformationParser: _router.routeInformationParser,
       routeInformationProvider: _router.routeInformationProvider,
       debugShowCheckedModeBanner: false,
-      theme: lightTheme,
-      darkTheme: darkTheme,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        colorScheme: ColorScheme.fromSeed(brightness: Brightness.light, seedColor: themeSeedColor),
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        colorScheme: ColorScheme.fromSeed(brightness: Brightness.dark, seedColor: themeSeedColor),
+      ),
     );
   }
 }
